73ae418257fdea4051b264ae12fd92ef92dd4f21
Philipp Stracker <stracker.phil@gmail.com>
2016-11-28 16:31:07 +0100

    Update snippet1.js

--- snippet1.js
+++ snippet1.js
@@ -1,6 +1,6 @@
 // ---- START OF MODIFICATION ---- ##CHANGE1
 // Add a dummy Preview-Module for all custom modules.
-var moduleId = 739;
+var moduleId = 743; // for Divi 3.0.21
 
 var specialModules = ['accordion-item','map-pin','section','social-media_follow','social-media_follow_network','column','tab','video-slider_item','fullwidth-post_slider','fullwidth-post_title','row-inner','row'];
 var def = function(e){return e&&e.__esModule?e:{default:e}}
